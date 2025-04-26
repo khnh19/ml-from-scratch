4687eef49f2c55bce160b49a8a9ef23ff398e342
Khanh Nguyen <khnhn1901@gmail.com>
2025-04-26 21:03:01 +0700

    random forest

---
+++ RandomForest.py
@@ -0,0 +1,57 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from collections import Counter
+from DecisionTree import DecisionTree
+
+
+class RandomForest:
+    def __init__(self, n_trees=10, max_depth=10, min_samples_split=2, n_features=None):
+        self.n_trees = n_trees
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.trees = []
+        self.n_features = n_features
+
+    def fit(self, X, y):
+        self.trees = []
+        for _ in range(self.n_trees):
+            tree = DecisionTree(
+                max_depth=self.max_depth, min_samples_split=self.min_samples_split, n_features=self.n_features)
+            X_sample, y_sample = self._bootstrap_sample(X, y)
+            tree.fit(X_sample, y_sample)
+            self.trees.append(tree)
+
+    def _bootstrap_sample(self, X, y):
+        n_samples = X.shape[0]
+        idxs = np.random.choice(n_samples, n_samples, replace=True)
+        return X[idxs], y[idxs]
+
+    def _most_common(self, y):
+        counter = Counter(y)
+        return counter.most_common(1)[0][0]
+
+    def predict(self, X):
+        predictions = np.array([tree.predict(X) for tree in self.trees])
+        tree_preds = np.swapaxes(predictions, 0, 1)
+        predictions = np.array([self._most_common(tree_pred)
+                               for tree_pred in tree_preds])
+        return predictions
+
+
+def main():
+    X, y = datasets.load_breast_cancer(return_X_y=True)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=86)
+
+    clf = RandomForest(n_trees=20)
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+
+    acc = np.sum(predictions == y_test) / len(y_test)
+    print(acc)
+
+
+if __name__ == '__main__':
+    main()
