a4022d49fa77b610f87781e0dc7646c656fe5f95
Khanh Nguyen <khnhn1901@gmail.com>
2025-04-27 10:36:40 +0700

    perceptron

---
+++ Perceptron.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+
+def unit_step(x):
+    return np.where(x > 0, 1, 0)
+
+
+class Perceptron:
+    def __init__(self, lr=0.01, n_iters=1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.activation_func = unit_step
+        self.weights = None
+        self.bias = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+
+        # init w and b
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        y_ = np.where(y > 0, 1, 0)
+
+        # learning
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                linear_output = np.dot(x_i, self.weights) + self.bias
+                y_predicted = self.activation_func(linear_output)
+
+                # update rule
+                update = self.lr * (y_[idx] - y_predicted)
+
+                self.weights += update * x_i
+                self.bias += update
+
+    def predict(self, X):
+        linear_output = np.dot(X, self.weights) + self.bias
+        y_predicted = self.activation_func(linear_output)
+        return y_predicted
+
+
+def main():
+    X, y = datasets.make_blobs(
+        n_samples=150, n_features=2, centers=2, cluster_std=1.05, random_state=2)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=86)
+
+    p = Perceptron(lr=0.01, n_iters=1000)
+    p.fit(X_train, y_train)
+    predictions = p.predict(X_test)
+
+    acc = np.sum(predictions == y_test) / len(y_test)
+    print(acc)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    plt.scatter(X_train[:, 0], X_train[:, 1], marker='o', c=y_train)
+
+    x0_1 = np.amin(X_train[:, 0])
+    x0_2 = np.amax(X_train[:, 0])
+
+    x1_1 = (-p.bias - p.weights[0] * x0_1) / p.weights[1]
+    x1_2 = (-p.bias - p.weights[0] * x0_2) / p.weights[1]
+
+    ax.plot([x0_1, x0_2], [x1_1, x1_2], 'k')
+
+    ymin = np.amin(X_train[:, 1])
+    ymax = np.amax(X_train[:, 1])
+    ax.set_ylim(ymin - 1, ymax + 1)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
